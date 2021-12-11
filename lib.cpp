1a5e973bf2433be36b2b2b9032935b0e22d375a4
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-11 15:15:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include <cstring>
+#include "lib.h"
+using namespace std;
+
+bool find (char array1[10][20], char array2[1][20], int &pos) {
+    for (int i=0; i<10; i++){
+        if (strcmp (array1[i], array2[0]) == 0){
+            pos = i;
+            return true;
+        }
+    }
+    return false;
+}
