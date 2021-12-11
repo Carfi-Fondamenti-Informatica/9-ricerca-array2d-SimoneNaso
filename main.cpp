e13b7d21ab94cb2d89b0f2922be8659d9fc1fd12
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-11 15:16:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main() {
+    int pos;
+    char a[10][20], b[1][20];
+    cout << "Inserisci i nomi propri da mettere nell'insieme" << endl;
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    cout << "Inserisci il nome proprio" << endl;
+    cin >> b[0];
+
+    if (find(a, b, pos)) {
+        cout << pos << endl;
+    } else {
+        cout << "non presente" << endl;
+    }
+    return 0;
 }
